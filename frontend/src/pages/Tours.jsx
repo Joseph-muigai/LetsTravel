4ad6c97f5883131f4e0417210a16f7f576c7cd57
Joseph-muigai <wambichojoseph@gmail.com>
2023-06-03 13:38:56 +0300

    create tours page and handle pagination on the tourcards

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,7 +1,55 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import CommonSection from "../shared/CommonSection";
+import "../styles/tour.css";
+import { Container, Row, Col } from "reactstrap";
+import NewsLetter from "../shared/NewsLetter";
+import TourCard from "../shared/TourCard";
+import SearchBox from "../shared/SearchBox";
+import tourData from "../assets/data/tours";
 const Tours = () => {
-  return <div>Tours</div>;
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    setPageCount(Math.ceil(5 / 4));
+  }, [page]);
+  return (
+    <>
+      <CommonSection title={"All Tours"} />
+      <section className="">
+        <Container>
+          <Row>
+            <SearchBox />
+          </Row>
+        </Container>
+      </section>
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {tourData?.map((tour) => (
+              <Col lg="3" className="mb-4" key={tour.id}>
+                <TourCard tour={tour} />
+              </Col>
+            ))}
+            <Col lg="12">
+              <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                {[...Array(pageCount).keys()].map((number) => (
+                  <span
+                    key={number}
+                    onClick={() => setPage(number)}
+                    className={page == number ? "active__page" : ""}
+                  >
+                    {number + 1}
+                  </span>
+                ))}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <NewsLetter />
+    </>
+  );
 };
 
 export default Tours;
